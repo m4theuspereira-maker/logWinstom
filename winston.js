ab429dea06266f52e7108937362f4f9a0f1fe015
m4theuspereira-maker <matheusmonaco123@gmail.com>
2020-12-13 14:59:42 -0300

    loggers

--- winston.js
+++ winston.js
@@ -4,9 +4,12 @@ const logger = winston.createLogger({
     level:'info', 
     format:winston.format.json(),
     transports:[
-        new winston.transports.Console()
+        new winston.transports.Console(),
+        new winston.transports.File({filename:'error.log', level:'error'}),
+        new winston.transports.File({filename:'combined.log'})
     ]
+    
 })
 
 
-logger.info(console.log('bem-vindo ao mundo da AIDS'))
+logger.info('bem-vindo ao mundo da AIDS')
